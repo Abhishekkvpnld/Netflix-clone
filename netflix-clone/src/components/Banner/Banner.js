328b1d5e812e9f0fa478b48d6001e03a8fdbaf3b
Abhishekkv <abhishekkvpnld@gmail.com>
2023-10-16 20:43:23 +0530

    navbar and  dummy posters are created

---
+++ netflix-clone/src/components/Banner/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Banner.css"
+
+function Banner() {
+  return (
+    <div className='banner'>
+<div className='contents'>
+<h1 className='title'>movie title</h1>
+<div className='banner-buttons'>
+<button className='button'>play</button>
+<button className='button'>my list</button>
+</div>
+<h1 className='description'>ugifaljfljkfsafffffffffffkswafsfuioffwse;e;afoffieeeee</h1>
+</div>
+<div className="fade"></div>
+
+    </div>
+  )
+}
+
+export default Banner
