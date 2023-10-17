19f8744c75116d6f7a0d36eaa56156fdd91df4d4
Abhishekkv <abhishekkvpnld@gmail.com>
2023-10-17 16:40:08 +0530

    different movie rows are created and provided different segment of data

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Banner from './components/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
+import { Top_Rated,Upcoming,Now_Playing,Popular } from './urls/url';
 
 
 function App() {
   return (
     <div className="App">
-<Navbar/>
-<Banner/>
-<RowPost/>
+      <Navbar />
+      <Banner />
+      <RowPost url={Top_Rated} title={'Netflix Originals'} />
+      <RowPost url={Now_Playing} title={'Action'} isSmall/>
+      <RowPost url={Upcoming} title={'Upcoming'} isSmall/>
+      <RowPost url={Popular} title={'Popular'} isSmall/>
     </div>
   );
 }
--- netflix-clone/src/components/RowPost/RowPost.js
+++ netflix-clone/src/components/RowPost/RowPost.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import "./RowPost.css"
 import axios from "../../axios/axios"
-import { API_KEY, imageUrl } from '../../constants/constants';
+import { imageUrl } from '../../constants/constants';
 
-function RowPost() {
+function RowPost(props) {
   const [Movie, setMovie] = useState([])
   useEffect(() => {
-    axios.get(`movie/now_playing?api_key=${API_KEY}`).then((Response) => {
+    axios.get(props.url).then((Response) => {
       setMovie(Response.data.results)
     })
   }, [])
 
   return (
     <div className='row'>
-      <h2>Netflix Originals</h2>
+      <h2>{props.title}</h2>
       <div className="posters">
         {
           Movie.map((obj) =>
 
-              <img className='poster' src={`${imageUrl+obj.backdrop_path}`} alt="image"  />
+              <img className={props.isSmall ? 'smallPoster' : 'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="image"  />
 
           )
         }
