eb0667ef7c91a7da63499832694d2d21010f9a9a
Abhishekkv <abhishekkvpnld@gmail.com>
2023-11-20 00:20:41 +0530

    Updates

--- netflix-clone/src/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl='https://api.themoviedb.org/3';
-export const API_KEY="5f3503067bc7e48a926fb30bcfbfd27b"
-export const imageUrl='https://image.tmdb.org/t/p/original'
---
+++ precache-manifest.4eb12825c6ef1e17d747c532d5650ec4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bd4d84b19c204774fb2c4e1509a526",
+    "url": "/netflix-host/index.html"
+  },
+  {
+    "revision": "2a498598690737669145",
+    "url": "/netflix-host/static/css/main.16c64d61.chunk.css"
+  },
+  {
+    "revision": "043e4778170f1c433df0",
+    "url": "/netflix-host/static/js/2.25cb3723.chunk.js"
+  },
+  {
+    "revision": "001c47af4bef020dd11796767485da4f",
+    "url": "/netflix-host/static/js/2.25cb3723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a9c9e73e623e7a3308",
+    "url": "/netflix-host/static/js/3.6bef7484.chunk.js"
+  },
+  {
+    "revision": "2a498598690737669145",
+    "url": "/netflix-host/static/js/main.8e377085.chunk.js"
+  },
+  {
+    "revision": "28b471920a9bc7281f09",
+    "url": "/netflix-host/static/js/runtime-main.d33c64eb.js"
+  }
+]);
